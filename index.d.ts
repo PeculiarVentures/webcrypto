6af89134f0c36d295b4090cabf83219a912a73a1
microshine <microshine@mail.ru>
2019-02-14 13:58:38 +0300

    Update d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,13 @@
-type NativeCrypto = Crypto;
-type NativeSubtleCrypto = SubtleCrypto;
-type NativeCryptoKey = CryptoKey;
+import * as core from "webcrypto-core";
 
-declare namespace WebCrypto {
-
-  class Crypto implements NativeCrypto {
-    public subtle: SubtleCrypto;
-    public getRandomValues<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | null>(array: T): T;
-  }
-
-  class CryptoKey implements NativeCryptoKey {
-    public algorithm: KeyAlgorithm;
-    public extractable: boolean;
-    public type: KeyType;
-    public usages: KeyUsage[];
-  }
-
+export declare class Crypto implements core.NativeCrypto {
+  public subtle: SubtleCrypto;
+  public getRandomValues<T extends Int8Array | Int16Array | Int32Array | Uint8Array | Uint16Array | Uint32Array | Uint8ClampedArray | Float32Array | Float64Array | DataView | null>(array: T): T;
 }
 
-export = WebCrypto;
+export declare class CryptoKey implements core.NativeCryptoKey {
+  public algorithm: KeyAlgorithm;
+  public extractable: boolean;
+  public type: KeyType;
+  public usages: KeyUsage[];
+}
