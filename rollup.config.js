bdb218481e9e9935e422446193cb1c7cd6cefbd4
microshine <microshine@mail.ru>
2019-12-16 16:27:50 +0300

    Update deps - add Copyright - Support ES module

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,39 @@
-import typescript from "rollup-plugin-typescript";
-
+import typescript from "rollup-plugin-typescript2";
 const pkg = require("./package.json");
 
-const banner = [pkg.banner || "", ""];
+const banner = [
+  "/**",
+  " * Copyright (c) 2019, Peculiar Ventures, All rights reserved.",
+  " */",
+  "",
+].join("\n");
+const input = "src/index.ts";
+const external = Object.keys(pkg.dependencies);
 
 export default {
-    input: "src/index.ts",
-    plugins: [
-        typescript({ typescript: require("typescript"), target: "esnext", removeComments: true }),
-    ],
-    external: ["crypto", ...Object.keys(pkg.dependencies)],
-    output: [
-        {
-            banner: banner.join("\n"),
-            file: pkg.main,
-            format: "cjs",
+  input,
+  plugins: [
+    typescript({
+      check: true,
+      clean: true,
+      tsconfigOverride: {
+        compilerOptions: {
+          module: "ES2015",
         }
-    ]
+      }
+    }),
+  ],
+  external: ["crypto", ...external],
+  output: [
+    {
+      banner,
+      file: pkg.main,
+      format: "cjs",
+    },
+    {
+      banner,
+      file: pkg.module,
+      format: "es",
+    },
+  ],
 };
